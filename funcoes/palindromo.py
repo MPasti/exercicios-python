35114cd92322227beb2f3f5d2b49f711d8afb22c
Matheus Pasti <117304327+MPasti@users.noreply.github.com>
2023-11-01 20:16:02 -0300

    Update palindromo.py

--- funcoes/palindromo.py
+++ funcoes/palindromo.py
@@ -1,8 +1,10 @@
+def palindromo(valor):
+    txt = valor[::-1]
+    if(txt == valor):
+        print("É um palíndromo!")
+    else:
+        print("Não é um palíndromo")
+
 print("Entre uma palavra, frase ou número para verificar se é um palíndromo")
 valor = input("Entre um valor ").lower()
-
-txt = valor[::-1]
-if(txt == valor):
-    print("É um palíndromo!")
-else:
-    print("Não é um palíndromo")
+palindromo(valor)
